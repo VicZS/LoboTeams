f32ff09c757fb03f03b36ef437366625de430722
VicZS <vicentezavsan@gmail.com>
2024-11-11 00:01:56 -0600

    Calendario Completo

--- src/app/pages/calendario/calendario.page.ts
+++ src/app/pages/calendario/calendario.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ModalController } from '@ionic/angular';
+import { DetalleActividadComponent } from 'src/app/components/detalle-actividad/detalle-actividad.component';
+import { Asignacion, ClaseMisAsignaciones } from 'src/app/interfaces';
 import { CApisService } from 'src/app/services/capis.service';
 
 @Component({
@@ -8,11 +11,12 @@ import { CApisService } from 'src/app/services/capis.service';
 })
 export class CalendarioPage implements OnInit {
 
-  constructor(private cliente:CApisService) { }
+  constructor(private modalCtr:ModalController, private cliente:CApisService, private alert: AlertController) { }
 
   ngOnInit() {
     setTimeout(() => {
       this.SesionAbierta();
+      this.obtenerTodasActividadesDeTodasLasClases();
     }, 500);
 
     return;
@@ -32,27 +36,141 @@ export class CalendarioPage implements OnInit {
 
   }
 
+  
+
+  async MostrarDetalle(actividadAux:ClaseMisAsignaciones){
+
+    //console.log(actividadAux);
+    
+    var AsignacionAux : Asignacion = {
+      "id": actividadAux.Asignacion_id,
+      "name": actividadAux.Nombre,
+      "descripcion": actividadAux.Descripción,
+      "date": actividadAux.Fecha,
+      "time": "na"
+    }
+
+    console.log(AsignacionAux);
+
+    this.MostrarDetallesActividad(AsignacionAux);
+  }
+
+  async MostrarDetallesActividad(actividad:Asignacion){
+    console.log(actividad);
+    const modal = await this.modalCtr.create({
+      component: DetalleActividadComponent,
+      componentProps:{
+        actividad
+      }
+    });
+    modal.present();
+  }
+
+  ActividadesClase: ClaseMisAsignaciones[] = [];
+
+  fechasAntesHoy: string = "";
+  fechasHoy: string = "";
+  fechasDespuesHoy: string = "";
   highlightedDates = [
     {
-      date: '2024-11-05',
-      textColor: '#800080',
-      backgroundColor: '#ffc0cb',
-    },
-    {
-      date: '2024-11-10',
-      textColor: '#09721b',
-      backgroundColor: '#c8e5d0',
-    },
-    {
-      date: '2024-11-20',
-      textColor: 'var(--ion-color-secondary-contrast)',
-      backgroundColor: 'var(--ion-color-secondary)',
-    },
-    {
-      date: '2024-11-23',
-      textColor: 'rgb(68, 10, 184)',
-      backgroundColor: 'rgb(211, 200, 229)',
-    },
+      date:'',
+      textColor:'',
+      backgroundColor:''
+    }
   ];
+
+
+  // async obtenerTodasActividadesDeTodasLasClases(){
+  //   console.log("Se obtendran todas las actividades de la clases");
+
+  //   var token = await this.cliente.obtenerToken();
+  //   const hoy = new Date().toISOString().split("T")[0];
+
+  //   this.cliente.PostObtenerTodasMisActividadesAsignadas(token).subscribe(
+  //     response => {
+
+  //       //console.log(response);
+  //       this.ActividadesClase = response.Clases;
+  //       console.log(this.ActividadesClase);
+
+  //       this.ActividadesClase.forEach(actividad => {
+  //         const fechaActividad = actividad.Fecha;
+
+  //         if (fechaActividad < hoy && !this.fechasAntesHoy.includes(fechaActividad)) {
+  //             this.fechasAntesHoy = this.fechasAntesHoy + fechaActividad +",";
+  //         } else if (fechaActividad === hoy && !this.fechasHoy.includes(fechaActividad)) {
+  //             this.fechasHoy = this.fechasHoy + fechaActividad +",";
+  //         } else if (fechaActividad > hoy && !this.fechasDespuesHoy.includes(fechaActividad)) {
+  //             this.fechasDespuesHoy = this.fechasDespuesHoy + fechaActividad +",";
+  //         }
+  //       });
+  //       console.log("Fechas anteriores a hoy:", this.fechasAntesHoy);
+  //       console.log("Fechas iguales a hoy:", this.fechasHoy);
+  //       console.log("Fechas posteriores a hoy:", this.fechasDespuesHoy);
+        
+  //     },
+  //     error =>{
+  //       console.error("Error cargar las Actividades");
+  //       console.log(error)
+  //     }
+  //   )
+  // }
+
+  async obtenerTodasActividadesDeTodasLasClases() {
+    console.log("Se obtendrán todas las actividades de las clases");
+
+    const token = await this.cliente.obtenerToken();
+    const hoy = new Date().toISOString().split("T")[0];
+
+    this.cliente.PostObtenerTodasMisActividadesAsignadas(token).subscribe(
+        response => {
+            this.ActividadesClase = response.Clases;
+
+            // Limpieza de highlightedDates para evitar duplicados
+            this.highlightedDates = [];
+
+            // Clasificación y asignación de colores según la fecha
+            this.ActividadesClase.forEach(actividad => {
+                const fechaActividad = actividad.Fecha;
+
+                // Verificar si la fecha ya existe en highlightedDates
+                if (!this.highlightedDates.some(item => item.date === fechaActividad)) {
+                    let textColor = '';
+                    let backgroundColor = '';
+
+                    if (fechaActividad < hoy) {
+                        // Fechas anteriores a hoy
+                        textColor = '#8a0000';
+                        backgroundColor = '#faaaaa';
+                    } else if (fechaActividad === hoy) {
+                        // Fechas iguales a hoy
+                        textColor = '#007cff';
+                        backgroundColor = '#8dc3fd';
+                    } else {
+                        // Fechas posteriores a hoy
+                        textColor = '#36a000';
+                        backgroundColor = '#ddfecc';
+                    }
+
+                    // Agregar la fecha con los colores correspondientes
+                    this.highlightedDates.push({
+                        'date': fechaActividad,
+                        'textColor': textColor,
+                        'backgroundColor': backgroundColor
+                    });
+                }
+            });
+
+            console.log("Fechas destacadas:", this.highlightedDates);
+        },
+        error => {
+            console.error("Error al cargar las Actividades");
+            console.log(error);
+        }
+    );
+}
+
+
+  
 
 }
